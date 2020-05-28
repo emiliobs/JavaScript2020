7f9b87e2902d228340a679d4c798c6732195d9c4
Emilio Barrea <barrera_emilio@yahoo.es>
2020-05-28 12:04:15 +0100

    All abaout Objeto console

--- basicos.js
+++ basicos.js
@@ -16,3 +16,66 @@ console.log(document);
 console.dir(window);
 console.dir(document);
 console.clear();
+
+console.group("Curso de @emiliobarrera");
+console.log("Curso de javascript");
+console.log("Curso de Node.js");
+console.log("Curso de  PWAs");
+console.log("Curso de Flexobox");
+console.log("Curso de diseo y progrmación web");
+console.groupEnd();
+
+console.groupCollapsed("Curso de @emiliobarrera");
+console.log("Curso de javascript");
+console.log("Curso de Node.js");
+console.log("Curso de  PWAs");
+console.log("Curso de Flexobox");
+console.log("Curso de diseo y progrmación web");
+console.groupEnd();
+
+console.clear();
+
+console.log(console);
+console.table(Object.entries(console).sort());
+
+const numeros = [1,2,3,4,5], vocales = ["a","e","i","o","u"];
+console.table(numeros);
+console.table(vocales);
+
+const perro = 
+{
+	 nombre: "Emilio",
+	 raza: "Boxer",
+	 color:"cafe"	
+
+}
+
+console.table(perro);
+console.clear();
+
+console.time("Cuanto tiempo tarda mi código");
+const arreglo = Array(1000000);
+for (let i = 0; i < Array.length; i++) 
+{
+	   arreglo[i] = i;
+	
+}
+
+console.timeEnd("Cuanto tiempo tarda mi código");
+console.log(arreglo);
+
+console.clear();
+
+for (let i = 0; i < 100; i++) {
+
+	console.count("Código for");
+	console.log(i);
+
+}
+
+console.clear();
+
+	let x =5,
+	 y = 2; 
+	let pruebaXY = "Se espera que X sea menor que Y";
+console.assert(x < y,{x,y, pruebaXY});
