778b84dbe20612e462a0fa077f1186a30788ebd3
Emilio Barrea <barrera_emilio@yahoo.es>
2020-05-31 12:20:21 +0100

    Objecto Math

--- basicos.js
+++ basicos.js
@@ -1,38 +1,20 @@
-console.log(Date());
-let fecha = new  Date();
-console.log(fecha);
-//día del mes
-console.log(fecha.getDate());
-//día de la semana -> D L M Mi J V S ->0 1 2 3 4 5 6
-console.log(fecha.getDay());
-//Mes Ene Feb Mar Abr May Jun Ago Sep Act Nov
-//MAyo -> 0 1 2 3 4 5 6 7 8 9 10 11
-console.log(fecha.getMonth());
-console.log(fecha.getFullYear());
-console.log(fecha.getHours());
-console.log(fecha.getMinutes());
-console.log(fecha.getSeconds());
-console.log(fecha.getMilliseconds());
-console.log(fecha.toString());
-console.log(fecha.toDateString())
-console.log(fecha.toLocaleString());
-console.log(fecha.toLocaleDateString());
-console.log(fecha.toLocaleTimeString());
-console.log(fecha.getTimezoneOffset());
-console.log(fecha.getUTCDate())
-console.log(fecha.getUTCHours());
-console.log(fecha.getTime());
-console.log(Date.now());
-let cumpleEmilio = new Date(1975,04,09);
-console.log(cumpleEmilio);
-
-
-
-
-
-
-
-
+console.log(Math);
+console.log(Math.PI);
+//VAlor absolucto:
+console.log(Math.abs(-7.8));
+//Para redondiar el número a mayor:
+console.log(Math.ceil(5.5));
+//Para redondiar el número a menor:
+console.log(Math.floor(5.5));
+//Para redondiar al mas cercano número:
+console.log(Math.round(5.5));
+console.log(Math.sqrt(81));
+console.log(Math.pow(5,5));
+//Indica un número positivo o negativo
+console.log(Math.sign(-5.5));
+console.log(Math.sign(-0.5));
+console.log(Math.random());
+console.log(Math.round(Math.random () * 1000));
 
 
 
