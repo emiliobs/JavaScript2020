cf75c67dfe66c587e7c0555afa5238c1c0b45b88
Emilio Barrea <barrera_emilio@yahoo.es>
2020-06-04 15:10:46 +0100

    alert, confirm y prompt

--- basicos.js
+++ basicos.js
@@ -1,39 +1,16 @@
-/*
-	Cortocircuito OR - cuando el valor de  la izquierda en la expresion siempre puede validar al true, es el valor que se cargará siempre puede validar a true, es el valor que se cargarra por por defecto.
-	
-	Cortocircuito AND - cuando el valor de a izquierda en la expresion siempre puede validar a false, es el valor que se cargaraá por defecto.
-*/
-
-function  saludar(nombre ) {
-	nombre = nombre || "Desconocido"
-	
-	console.log(`Hola ${nombre}`);
-	
-}
-saludar("Emilio");
-saludar();
-console.log("cadena" || "valor de la cadena");
-console.log(55 || "valor de la cadena");
-console.log(true || "valor de la cadena");
-console.log([] || "valor de la cadena");
-console.log({} || "valor de la cadena");
-console.log(false || "valor de la cadena");
-console.log(null || "valor de la cadena");
-console.log(undefined || "valor de la cadena");
-console.log("" || "valor de la cadena");
-console.log(-2 || "valor de la cadena");
-console.log(undefined && "valor de la cadena");
-console.log(false && "valor de la cadena");
-console.log(null && "valor de la cadena");
-console.log(undefined && "valor de la cadena");
-console.log("" && "valor de la cadena");
-console.log(-2 && "valor de la cadena");
-console.log("cadena"  && "valor de la cadena");
-console.log(55 && "valor de la cadena");
-console.log(true && "valor de la cadena");
-console.log([] && "valor de la cadena");
-console.log({} && "valor de la cadena");
+// console.log(window);
+// alert("Hola esto es una alerta");
+// confirm("Hola esto es una confirmation");
+// prompt("Hola esto es un prompt, le permite al usuario ingresar un valor.");
 
 
+let alerta = alert("Hola esto es una alerta");
+let confirmacion = confirm("Hola esto es una confirmation") ;
+ let aviso = prompt("Hola esto es un aviso, le permite al usuario ingresar un valor.");
 
-
+ console.log(alerta);
+ console.log(confirmacion);
+ console.log(aviso);
+ 
+ 
+ 
