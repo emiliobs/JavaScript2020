c054c4d9cc63e05ca530b2c0f1657af0b38cdd4f
Emilio <barrera_emilio@yahoo.es>
2020-06-06 12:22:25 +0100

    Expresiones Regulares

--- basicos.js
+++ basicos.js
@@ -1,16 +1,17 @@
-// console.log(window);
-// alert("Hola esto es una alerta");
-// confirm("Hola esto es una confirmation");
-// prompt("Hola esto es un prompt, le permite al usuario ingresar un valor.");
+/*
+Expresiones Regulares:
+Son una secuencia de caracteres que forman un patón de búsqueda, principalmente utilizada para la búsqueda de patrones de cadena de caracteres.
+*/
 
-
-let alerta = alert("Hola esto es una alerta");
-let confirmacion = confirm("Hola esto es una confirmation") ;
- let aviso = prompt("Hola esto es un aviso, le permite al usuario ingresar un valor.");
-
- console.log(alerta);
- console.log(confirmacion);
- console.log(aviso);
+let cadena = "Lorem ipsum emilio dolor sit amet 5 consectetur adipisicing elit. Dolores sunt similique itaque iure repellat dolore atque 36 earum id  55 ut praesentium, repudiandae provident Emilio quibusdam doloribus? Laboriosam emilio provident error dolores quos assumenda!";
  
- 
+// let expReg = new RegExp("dolores","ig");
+// console.log(expReg.test(cadena));
+// console.log(expReg.exec(cadena));
+
+let expReg2 = /emilio/ig;
+console.log(expReg2.test(cadena));
+console.log(expReg2.exec(cadena));
+
+
  
